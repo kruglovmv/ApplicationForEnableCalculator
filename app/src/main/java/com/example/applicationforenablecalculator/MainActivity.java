e81b6027d21629b2298c01a77e9bfda254a45035
Alexey 'KrYack <x.kryack.x@gmail.com>
2021-12-24 19:50:49 +0300

    End commit

--- app/src/main/java/com/example/applicationforenablecalculator/MainActivity.java
+++ app/src/main/java/com/example/applicationforenablecalculator/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.applicationforenablecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+MaterialButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButton(findViewById(R.id.button));
+    }
+
+    private void initButton(View viewById) {
+        viewById.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent("com.example.calculator.START"));
+            }
+        });
     }
 }
